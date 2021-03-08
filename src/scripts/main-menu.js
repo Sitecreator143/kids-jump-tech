09a1bccaf2849172e6502e4ff445e34da29b96c0
Alexander Nikolaev <nikolaevsailing@mail.ru>
2021-03-08 23:41:15 +0300

    feat: full main

--- src/scripts/main-menu.js
+++ src/scripts/main-menu.js
@@ -1,16 +1,20 @@
 export class Menu {
   constructor() {
+    this.body = document.querySelector("body");
     this.mainBurger = document.querySelector("[data-main-burger]");
-    this.cross = document.querySelector("[data-nav-cross]");
-    this.header = document.querySelector("[data-main-header]");
-
+    this.navCross = document.querySelector("[data-nav-cross]");
+    this.mainHeader = document.querySelector("[data-main-header]");
+    this.mainSocial = document.querySelector("[data-main-social]")
     this.mainBurger.addEventListener("click", () => {
-      this.header.classList.toggle("main__header_active");
+      this.body.classList.toggle("body_lock");
+      this.mainHeader.classList.toggle("main__header_active");
+      this.mainSocial.classList.toggle("main__social_active");
     });
 
-    this.cross.addEventListener("click", () => {
-      this.header.classList.toggle("main__header_active");
+    this.navCross.addEventListener("click", () => {
+      this.body.classList.toggle("body_lock");
+      this.mainHeader.classList.toggle("main__header_active");
+      this.mainSocial.classList.toggle("main__social_active");
     });
   }
-  
 }
